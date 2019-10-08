342a504873fd24d2ddc16fb19d6349af0523fa69
StasGill <stasgill@gmail.com>
2019-10-08 12:40:36 +0300

    red

--- hw-01/java/task-4.js
+++ hw-01/java/task-4.js
@@ -1,18 +1,27 @@
-// let credits = 23580;
-// let pricePerDroid = 3000;
-// let message;
+let credits = 23580;
+let pricePerDroid = 3000;
+let message;
 
-// let totalDroid = +prompt("Kоличество дроидов которые пользователь хочет купить");
+let totalDroid = +prompt("Kоличество дроидов которые пользователь хочет купить");
 
-// let totalPrice= totalDroid * pricePerDroid;
+let totalPrice= totalDroid * pricePerDroid;
 
+ if ( totalPrice <= credits && totalPrice !== 0  ) { { message = `Вы купили ${totalDroid} дроида`}
+    console.log(`Вы купили ${totalDroid} дроидов, на счету осталось ${credits - totalPrice} кредитов.`);
+  }
+   
+  else if( totalPrice > credits ) {message = 'Недостаточно средств на счету!'}
 
-
-// if ( totalPrice <= credits) { { message = `Вы купили ${totalDroid} дроида`};
-//     console.log(`Вы купили ${totalDroid} дроидов, на счету осталось ${credits - totalPrice} кредитов.`);
-//   }
-//   else  if(totalDroid === null) {message = "Отменено пользователем"}
-
-//   else { message = 'Недостаточно средств на счету!'};
+  else  { message = "Отменено пользователем"}
   
-//   alert(message);
+  
+
+
+
+
+
+
+  alert(message);
+
+
+  
