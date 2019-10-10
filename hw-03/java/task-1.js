790e8b006a38c9d01898d24ea3908c2f38a2d79e
StasGill <stasgill@gmail.com>
2019-10-10 09:54:45 +0300

    hw3newch

--- hw-03/java/task-1.js
+++ hw-03/java/task-1.js
@@ -1,27 +1,32 @@
+function newUser(object) {
 
-// function newUser(object){
-
-//   object.mood = 'happy'
-//   object.hobby = 'javascript'
-//   
-//    object.premium = false
-  
-//   return object 
-// }
+  object.mood = 'happy'
+  object.hobby = 'javascript'
+  object.premium = false
+const keys = Object.keys(object);
 
 
+  for (const key of keys){
+    `${key}: ${object[key]}`
+  }
 
-
-
-
+  return object
+}
 
 
 
-// const user = {
-//   name: 'Mango',
-//   age: 20,
-//   hobby: 'html',
-//   premium: true,
-// };
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
 
-// console.log(newUser(user));
+const user1 = {
+  name: 'Alex',
+  age: 25,
+  hobby: 'htm',
+  premium: false,
+};
+
+console.log(newUser(user));
