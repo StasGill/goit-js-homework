878f472c9a09893d392642f7c458060a02afb54f
StasGill <stasgill@gmail.com>
2019-10-23 12:24:24 +0300

    hw5

--- hw-05/index.js
+++ hw-05/index.js
@@ -117,7 +117,8 @@
   
 // ===================================================================================================
 // Задание 4
-// Напиши класс StringBuilder. На вход он получает один параметр - строку, которую записывает в свойство _value.
+// Напиши класс StringBuilder. На вход он получает один параметр - строку, которую записывает 
+// в свойство _value.
 
 // Добавь классу следующий функционал:
 
@@ -137,36 +138,57 @@
 // console.log(builder.value; // '=^.^='
 // ===================================================================================================
 
-class StringBuilder {
-    constructor(items) { this.items = items;}
-    let value = ''
-    append(str)
-    prepend(str)
-    pad(str)
-}
-const builder = new StringBuilder(stringbuilder)
+// class StringBuilder {
+//   constructor(str) {
+//     this._value = str;
+//   }
+// ​
+//   get value() {
+//     return this._value;
+//   }
+// ​
+//   append(str) {
+//     return (this._value = `${this.value}${str}`);
+//   }
+// ​
+//   prepend(str) {
+//     return (this._value = `${str}${this.value}`);
+//   }
+// ​
+//   pad(str) {
+//     return (this._value = `${str}${this.value}${str}`);
+//   }
+// }
+// ​
+// const builder = new StringBuilder(".");
+// console.log("builder", builder);
+// ​
+// builder.append("^");
+// console.log(builder.value); // '.^'
+// ​
+// builder.prepend("^");
+// console.log(builder.value); // '^.^'
+// ​
+// builder.pad("=");
+// console.log(builder.value); // '=^.^='
+// Collapse
 
-builder.append('^');
-console.log(builder.value); // '.^'
 
-builder.prepend('^');
-console.log(builder.value; // '^.^'
 
-builder.pad('=');
-console.log(builder.value; // '=^.^='
+
 
 // ===================================================================================================
 // Задание 5
 // Напиши класс Car с указанными свойствами и методами.
-
+​
 // class Car {
-
-  //  * Добавь статический метод `getSpecs(car)`,
-  //  * который принимает объект-машину как параметр и выводит
-  //  * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
-   
-
-
+//   /*
+//    * Добавь статический метод `getSpecs(car)`,
+//    * который принимает объект-машину как параметр и выводит
+//    * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
+//    */
+​
+//   /*
 //    * Конструктор получает объект настроек.
 //    *
 //    * Добавь свойства будущеего экземпляра класса:
@@ -177,72 +199,191 @@ console.log(builder.value; // '=^.^='
 //    *  distance - общий киллометраж, изначально 0
 //    */
 //   constructor() {}
-
-
+​
+//   /*
 //    * Добавь геттер и сеттер для свойства price,
 //    * который будет работать с свойством цены автомобиля.
 //    */
-
-
+​
+//   /*
 //    * Добавь код для того чтобы завести автомобиль
 //    * Записывает в свойство isOn значение true
 //    */
 //   turnOn() {}
-
-//   
+​
+//   /*
 //    * Добавь код для того чтобы заглушить автомобиль
 //    * Записывает в свойство isOn значение false,
 //    * и сбрасывает текущую скорость в 0
 //    */
 //   turnOff() {}
-
-//   
+​
+//   /*
 //    * Добавялет к свойству speed полученное значение,
 //    * при условии что результирующая скорость
 //    * не больше чем значение свойства maxSpeed
 //    */
 //   accelerate(value) {}
-
-//   
+​
+//   /*
 //    * Отнимает от свойства speed полученное значение,
 //    * при условии что результирующая скорость не меньше нуля
 //    */
 //   decelerate(value) {}
-
-//   
+​
+//   /*
 //    * Добавляет в поле distance киллометраж (hours * speed),
 //    * но только в том случае если машина заведена!
 //    */
 //   drive(hours) {}
 // }
-
+​
 // const mustang = new Car({ maxSpeed: 200, price: 2000 });
-
+​
 // mustang.turnOn();
 // mustang.accelerate(50);
 // mustang.drive(2);
-
+​
 // Car.getSpecs(mustang);
-//  maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
-
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+​
 // mustang.decelerate(20);
 // mustang.drive(1);
 // mustang.turnOff();
-
+​
 // Car.getSpecs(mustang);
-//  maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
-
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 30, price: 2000
+​
 // console.log(mustang.price); // 2000
 // mustang.price = 4000;
 // console.log(mustang.price); // 4000
-
-// ===================================================================================================
-
-// static getSpecs(car) {
-//   const {maxSpeed, speed, isOn, distance, price} = car;
-//   console.log(`maxSpeed: ${maxSpeed}, speed: ${speed}, isOn: {isOn}, distance: ${distance}, price: ${price} `);
+​
+// Решение 5
+// class Car {
+//   /*
+//    * Добавь статический метод `getSpecs(car)`,
+//    * который принимает объект-машину как параметр и выводит
+//    * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
+//    */
+// ​
+//   static getSpecs(car) {
+//     const { maxSpeed, speed, isOn, distance, price } = car;
+//     console.log(
+//       `maxSpeed: ${maxSpeed}, speed: ${speed}, isOn: ${isOn}, distance: ${distance}, price: ${price}`
+//     );
+//   }
+// ​
+//   /*
+//    * Конструктор получает объект настроек.
+//    *
+//    * Добавь свойства будущеего экземпляра класса:
+//    *  speed - текущая скорость, изначально 0
+//    *  price - цена автомобиля
+//    *  maxSpeed - максимальная скорость
+//    *  isOn - заведен ли автомобиль, значения true или false. Изначально false
+//    *  distance - общий киллометраж, изначально 0
+//    */
+// ​
+//   constructor({ maxSpeed, price }) {
+//     this.maxSpeed = maxSpeed;
+//     this._price = price;
+//     this.speed = 0;
+//     this.distance = 0;
+//     this.isOn = false;
+//   }
+// ​
+//   /*
+//    * Добавь геттер и сеттер для свойства price,
+//    * который будет работать с свойством цены автомобиля.
+//    */
+// ​
+//   get price() {
+//     return this._price;
+//   }
+// ​
+//   set price(price) {
+//     this._price = price;
+//   }
+// ​
+//   /*
+//    * Добавь код для того чтобы завести автомобиль
+//    * Записывает в свойство isOn значение true
+//    */
+// ​
+//   turnOn(isOn) {
+//     this.isOn = true;
+//   }
+// ​
+//   /*
+//    * Добавь код для того чтобы заглушить автомобиль
+//    * Записывает в свойство isOn значение false,
+//    * и сбрасывает текущую скорость в 0
+//    */
+// ​
+//   turnOff(isOn) {
+//     this.isOn = false;
+//     this.speed = 0;
+//   }
+// ​
+//   /*
+//    * Добавялет к свойству speed полученное значение,
+//    * при условии что результирующая скорость
+//    * не больше чем значение свойства maxSpeed
+//    */
+// ​
+//   accelerate(value) {
+//     if (this.speed + value <= this.maxSpeed) {
+//       this.speed = this.speed + value;
+//     }
+//   }
+// ​
+//   /*
+//    * Отнимает от свойства speed полученное значение,
+//    * при условии что результирующая скорость не меньше нуля
+//    */
+// ​
+//   decelerate(value) {
+//     if (this.speed - value >= 0) {
+//       this.speed = this.speed - value;
+//     }
+//   }
+// ​
+//   /*
+//    * Добавляет в поле distance киллометраж (hours * speed),
+//    * но только в том случае если машина заведена!
+//    */
+// ​
+//   drive(hours) {
+//     if (this.isOn) {
+//       this.distance = this.speed * hours;
+//     }
+//   }
 // }
+// ​
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+// ​
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+// ​
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+// ​
+// mustang.decelerate(20);
+// mustang.drive(1);
+// mustang.turnOff();
+// ​
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 30, price: 2000
+// ​
+// console.log(mustang.price); // 2000
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
+// Collapse
 
-// constructor({maxSpeed, speed, isOn, distance, price})
+
+
+
+
 
 
