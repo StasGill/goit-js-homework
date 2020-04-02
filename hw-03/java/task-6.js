53fbaafdb30681ef41b6314bdbdf50dcc3eb69b1
StasGill <54024811+StasGill@users.noreply.github.com>
2020-04-02 17:54:42 +0300

    Update task-6.js

--- hw-03/java/task-6.js
+++ hw-03/java/task-6.js
@@ -26,3 +26,87 @@
 
 
 // console.log(getAllPropValues(products, 'Сканер'));
+
+//--------------------------------------------------------------------------------------------------------------------
+// Задание 7
+// Напиши скрипт управления личным кабинетом интернет банка. 
+// Есть объект account в котором необходимо реализовать методы для работы с балансом и историей транзакций.
+
+// /*
+//  * Типов транзацкий всего два.
+//  * Можно положить либо снять деньги со счета.
+//  */
+
+// };
+
+// /*
+//  * Каждая транзакция это объект со свойствами: id, type и amount
+//  */
+
+// const transactions = [ ]
+
+// const account = {
+//    id: 1,
+//    type:'deposit', 
+//    balance: 0,
+//    trBalance:0,
+//    createTransaction(amount, type) {
+//      this.balance +=amount
+//      this.trBalance =amount
+//      this.type =type
+//      this.id += 1
+//      transactions.push({...account})
+     
+//    },
+//    deposit(amount) {
+//       this. createTransaction(amount, 'deposit')
+//    },
+//    withdraw(amount) {
+//     this. createTransaction(amount, 'withdraw')
+//    },
+//    getBalance() {
+//      console.log(this.balance)
+//    },
+//    getTransactionDetails(id) {
+//     for(let key of transactions){
+//       for(let keyTwo in key){
+//         if(key[keyTwo] === id){
+//         console.log('Youre transaction',key)
+//                 }
+//                 }
+//             }
+
+//    },
+//    getTransactionTotal(type) {
+//      let totalSum = 0
+//      for(let key of transactions){
+       
+//        for(let keyTwo in key){
+        
+//         if(key[keyTwo] === type){
+        
+//           totalSum +=key.trBalance
+//           }
+//           }
+//          }
+//      return console.log('Общая сумма транзакций',totalSum)
+
+//    },
+
+//   }
+
+//   // account.createTransaction( 2, 'deposit') 
+//   account.deposit( 45 )
+//   // account.withdraw( -25 )
+//   account.deposit( 40 )
+//   account.deposit( 40 )
+//   account.withdraw( -25 )
+//   account.withdraw( -20 )
+//   account.getBalance()
+//   account.getTransactionDetails(3)
+//   account.getTransactionTotal('withdraw')
+//   console.log(transactions)
+
+//   const Transaction = {
+//     DEPOSIT: 'deposit',
+//     WITHDRAW: 'withdraw',}
